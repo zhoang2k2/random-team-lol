b281bcefebbe12584c396c647291321f38c17625
cmdn-hoangnguyen2 <hoang.nguyen@classmethod.vn>
2026-06-04 16:37:32 +0700

    Big update

--- build-spa.js
+++ build-spa.js
@@ -1,11 +1,11 @@
-import app from './dist/server/server.js';
-import fs from 'fs';
+import app from "./dist/server/server.js";
+import fs from "fs";
 
 async function generate() {
-  const req = new Request('http://localhost/');
+  const req = new Request("http://localhost/");
   const res = await app.fetch(req, {}, {});
   const html = await res.text();
-  fs.writeFileSync('dist/client/index.html', html);
-  console.log('SPA index.html generated successfully.');
+  fs.writeFileSync("dist/client/index.html", html);
+  console.log("SPA index.html generated successfully.");
 }
 generate().catch(console.error);
